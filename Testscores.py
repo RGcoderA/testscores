ccabf9e6aeffc5ca4c317b509fe2cd15a2fba6f6
R <108376638+RGcoderA@users.noreply.github.com>
2023-02-21 11:12:32 -0500

    Add files via upload

---
+++ Testscores.py
@@ -0,0 +1,28 @@
+# This program gets a numeric test score from the
+# user and displays the corresponding letter grade.
+
+# Variables to represent the grade thresholds
+A_score = 90
+B_score = 80
+C_score = 70
+D_score = 60
+
+# Get a test score from the user.
+score = (input('Enter your test score: '))
+ 
+# Determine the grade.
+# The error which I fix are the score putting them in quotes and the number that the only error
+if score >= "90":
+    print('Your grade is A.')
+else:
+    if score >= "80":
+        print('Your grade is B.')
+    else:
+        if score >= "70":
+            print('Your grade is C.')
+        else:
+            if score >= "60":
+                print('Your grade is D.')
+            else:
+                print('Your grade is F.')
+
